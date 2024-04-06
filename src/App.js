d42d7c2553499c5a73b94ba1c42ccf741164364c
Ivan Zou <ivanzou@seas.upenn.edu>
2024-04-06 15:24:34 -0400

    feat: Create boilerplate code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+    
+    return (
+        <div className="App">
+          <h1>Budget Patrol</h1>
+        </div>
+    );
+}
+
+export default App;
