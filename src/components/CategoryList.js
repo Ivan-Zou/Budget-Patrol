8214807032c5ff0e059e88f625af5b8cbc588ca2
Ivan Zou <ivanzou2@gmail.com>
2024-04-09 16:13:00 -0400

    feat: implemented category list

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,1 +1,19 @@
 import React, { useState } from 'react';
+
+const CategoryList = (props) => {
+
+    return (
+        <div >
+            <ul>
+                <li>Category</li>
+                <li>Allocated</li>
+                <li>Remaining</li>
+            </ul>
+            <ul className="category-list">
+                <li>{props.chidren}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default CategoryList;
