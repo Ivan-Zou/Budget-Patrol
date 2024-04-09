ea8c7b016c0253d770e849fc7c400421bc3c9370
Ivan Zou <ivanzou2@gmail.com>
2024-04-09 16:44:37 -0400

    feat: added state for categories

--- src/tabs/ViewBudgetTab.js
+++ src/tabs/ViewBudgetTab.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import CategoryList from '../components/CategoryList';
+import CategoryItem from '../components/CategoryItem';
 
 function ViewBudgetTab() {
-
+    const [categoryItems, setCategoryItems] = useState([
+        {
+            key: 0,
+            name: "",
+            allocated: 0,
+            remaining: 0,
+        }
+    ])
 
     return (
         <div>
             <h3>View Budget</h3>
+            <CategoryList>
+                
+            </CategoryList>
         </div>
     );
 }
