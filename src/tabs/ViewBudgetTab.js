2139b6f6a7c4f2dceed12824c181ae89c9bc6e10
Ivan Zou <ivanzou2@gmail.com>
2024-04-11 16:04:22 -0400

    feat: created area to input category and budget allocation

--- src/tabs/ViewBudgetTab.js
+++ src/tabs/ViewBudgetTab.js
@@ -3,6 +3,8 @@ import CategoryList from '../components/CategoryList';
 import CategoryItem from '../components/CategoryItem';
 
 function ViewBudgetTab() {
+    const [budget, setBudget] = useState(0);
+    const [category, setCategory] = useState("");
     const [categoryItems, setCategoryItems] = useState([
         {
             key: 0,
@@ -18,6 +20,17 @@ function ViewBudgetTab() {
             <CategoryList>
                 
             </CategoryList>
+            <div>
+                <h3>Add Category</h3>
+                <div id="category_input_container">
+                    <label>Category: </label><br/>
+                    <input id="category_input" type="text" placeholder="Enter Category" name="category" value={category} onInput={e => setCategory(e.target.value)}/>
+                </div>
+                <div id="budget_container">
+                    <label>Budget: $ </label><br/>
+                    <input id="budget_input" type="number" placeholder="0.00" name="budget" min="0" step="0.01" title="Currency" pattern="^\d*(\.\d{1,2})?$" value={budget} onInput={e => setBudget(e.target.value)}/>
+                </div>
+            </div>
         </div>
     );
 }
